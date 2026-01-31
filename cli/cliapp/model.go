eb6ad08b3fdd72fb92a8f7eb3b023ef669a85df0
Alberto Rivadulla <albertorivadullasanchez@gmail.com>
2026-01-31 22:26:05 +0100

    Implement the main menu screen in Bubbletea

--- cli/cliapp/model.go
+++ cli/cliapp/model.go
@@ -2,29 +2,49 @@ package cliapp
 
 import (
 	"context"
-	// "fmt"
+	"strings"
 
 	"carmaintenance/internal/core"
 
-	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/bubbles/list"
+    "github.com/charmbracelet/bubbles/table"
+    "github.com/charmbracelet/bubbles/viewport"
+    tea "github.com/charmbracelet/bubbletea"
+    "github.com/charmbracelet/lipgloss"
 )
 
 type Screen int
 
 const (
 	MainMenuScreen Screen = iota
-	SelectTablesScreen
-	ViewTableScreen
+	ViewTablesScreen
+	ManageTableScreen
 	AddEntryScreen
 	EditEntryScreen
 	SelectQueryScreen
 )
 
 type Model struct {
-	screen Screen
+	currScreen Screen
 
+	// Elements of the left column
+	mainMenuList list.Model
+	viewTablesMenuList list.Model
+	manageTableMenuList list.Model
+	runQueriesMenuList list.Model
 	cursor int
-	choices []string
+	// TODO: Menu for applying the rules
+
+	// Elements of the right column
+	table table.Model
+	viewport viewport.Model
+
+	width int
+	leftWidth int
+	rightWidth int
+	height int
+
+	ready bool
 
 	log []string
 
@@ -32,9 +52,48 @@ type Model struct {
 	backend *core.Backend
 }
 
+type menuItem struct {
+	title, desc string
+}
+
+func (i menuItem) Title() string { return i.title }
+func (i menuItem) Description() string { return i.desc }
+func (i menuItem) FilterValue() string { return i.title }
+
 func NewModel(ctx context.Context, backend *core.Backend) Model {
+	mainMenuItems := []list.Item{
+		menuItem{title: "Manage tables", desc: "Operate on the available tables"},
+		menuItem{title: "Run queries", desc: "Run one of the specified queries"},
+	}
+	mainMenuList := list.New(mainMenuItems, list.NewDefaultDelegate(), 0, 0)
+	mainMenuList.Title = "Main menu"
+	mainMenuList.InfiniteScrolling = true
+	mainMenuList.SetShowStatusBar(false)
+	mainMenuList.SetFilteringEnabled(true)
+
+	manageTableMenuItems := []list.Item{
+		menuItem{title: "View table", desc: "Show all the entries of the table"},
+		menuItem{title: "Add entry", desc: "Add a new entry to the table"},
+		menuItem{title: "Edit entry", desc: "Select an entry and edit or remove it"},
+	}
+	manageTableMenuList := list.New(manageTableMenuItems, list.NewDefaultDelegate(), 0, 0)
+	manageTableMenuList.Title = "Main menu"
+	manageTableMenuList.InfiniteScrolling = true
+	manageTableMenuList.SetShowStatusBar(false)
+	manageTableMenuList.SetFilteringEnabled(true)
+	// TODO: Set the title after selecting a table
+
+	// Viewport for text results
+	viewport := viewport.New(0, 0)
+
 	return Model{
-		screen: MainMenuScreen,
+		currScreen: MainMenuScreen,
+
+		mainMenuList: mainMenuList,
+		manageTableMenuList: manageTableMenuList,
+
+		viewport: viewport,
+
 		ctx: ctx,
 		backend: backend,
 	}
@@ -46,17 +105,24 @@ func (m Model) Init() tea.Cmd {
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	// TODO: Implement
+	var cmd tea.Cmd
+	var cmds []tea.Cmd
 
 	switch msg := msg.(type) {
-
 	case tea.KeyMsg:
-		switch msg.String() {
+		// TODO: Check if the current screen is in filter state, and ignore the keys
+		// Modify this
+		if m.mainMenuList.FilterState() == list.Filtering {
+			break
+		}
 
+		switch msg.String() {
 		case "ctrl+c", "q":
 			return m, tea.Quit
 
 		case "enter":
+			// TODO: Manage interaction with the different menus
+
 			// example DB interaction
 			// result := queryDB(m.ctx, m.db)
 			// m.log = append(m.log, result)
@@ -65,16 +131,67 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	case tea.WindowSizeMsg:
 		// handle resize if needed
+		m.width = msg.Width
+		m.height = msg.Height
+
+		// Column width
+		m.leftWidth = int(float64(m.width) * 0.4)
+		m.rightWidth = m.width - m.leftWidth - 2
+
+		m.mainMenuList.SetSize(m.leftWidth, m.height - 2)
+		m.manageTableMenuList.SetSize(m.leftWidth, m.height - 2)
+		m.viewport.Width = m.rightWidth
+		m.viewport.Height = m.height - 4
+
+		if !m.ready {
+			m.ready = true
+		}
 	}
 
-	return m, nil
+	// TODO: Update the currently active item in the left
+	// Modify this
+	m.mainMenuList, cmd = m.mainMenuList.Update(msg)
+	cmds = append(cmds, cmd)
+
+	// TODO: Update the currently active item in the right
+	// Modify this
+	m.viewport, cmd = m.viewport.Update(msg)
+	cmds = append(cmds, cmd)
+
+	return m, tea.Batch(cmds...)
 }
 
 func (m Model) View() string {
-	// TODO: Implement
-	s := "Press Enter to run query\nPress q or Ctrl+C to quit\n\n"
-	for _, line := range m.log {
-		s += line + "\n"
+	if !m.ready {
+		return "Intializing the CLI app..."
 	}
-	return s
+
+	// Define styles
+    columnStyle := lipgloss.NewStyle().
+        Padding(1)
+
+    leftColumnStyle := columnStyle.Width(int(float64(m.width) * 0.4))
+
+    rightColumnStyle := columnStyle.Width(m.width - int(float64(m.width)*0.4) - 2)
+
+	// TODO: Render left column
+	// Modify this
+    leftColumn := leftColumnStyle.Render(m.mainMenuList.View())
+
+	// TODO: Render left column
+	// Modify this
+    rightColumn := rightColumnStyle.Render(m.viewport.View())
+
+	// Combine the two columns
+	content := lipgloss.JoinHorizontal(
+		lipgloss.Top,
+		leftColumn,
+		rightColumn,
+	)
+
+    status := lipgloss.NewStyle().
+        Foreground(lipgloss.Color("241")).
+		Render(strings.Join(m.log, " - "))
+
+	return content + status
 }
